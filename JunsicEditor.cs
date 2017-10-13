90322c4940ba63693d4419e50fa87ac692d1f7c7
junsic Park <jun12041344@gmail.com>
2017-10-13 23:35:00 +0900

    Editor(TowerDefence)
    Start : 10-13

---
+++ JunsicEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine.UI;
+using System.Reflection;
+using UnityEditor.SceneManagement;
+
+public class JunsicEditor : EditorWindow
+{
+    private static JunsicEditor window;
+    [MenuItem("Window/JunSic Editor")]
+    public static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        window = (JunsicEditor)EditorWindow.GetWindow(typeof(JunsicEditor));
+        window.Show();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
